77796b904304cfe1ee00f4b0eed31db0e8c713a2
sarvar <asrorjon5538@gmail.com>
2021-02-25 16:35:35 +0500

    initial

--- js/main.js
+++ js/main.js
@@ -81,9 +81,6 @@ $('.responsive').slick({
                 slidesToScroll: 1
             }
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
     ]
 });
 
